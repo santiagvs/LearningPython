ac091f01fee32cb42353382060d335a46f94ddea
Santiago <victor.lacsilva0102@gmail.com>
2022-11-29 22:04:39 -0300

    feat: starting python things

---
+++ forca.py
@@ -0,0 +1,10 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo no jogo de forca!***")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+
+if __name__ == "__main__":
+    jogar()
