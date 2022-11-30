cfb924ec159f87975f55470c86a9dda82358157f
Santiago <victor.lacsilva0102@gmail.com>
2022-11-29 23:34:36 -0300

    feat: adding new functionalities in hangman game

--- forca.py
+++ forca.py
@@ -3,6 +3,20 @@ def jogar():
     print("***Bem vindo no jogo de forca!***")
     print("*********************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while not enforcou and not acertou:
+        chute = input("Qual letra? ")
+
+        index = 0
+        for letra in palavra_secreta:
+            if chute == letra:
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index += 1
+        print("jogando")
+
     print("Fim do jogo")
 
 
